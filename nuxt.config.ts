441ca631e3f0a04ef50378d3ffe9bcb022eceea1
Kusa Mochi <whoatemyapplepie@gmail.com>
2024-04-14 18:01:45 +0900

    config nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    baseURL: ".",
+  },
   devtools: { enabled: true },
   modules: [
     'vuetify-nuxt-module'
   ],
+  ssr: false,
 })
