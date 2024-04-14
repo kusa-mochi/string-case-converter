964360cac47ee3855553fa0b633b135f758b19cb
Kusa Mochi <whoatemyapplepie@gmail.com>
2024-04-14 11:56:08 +0900

    remove unused code.

--- components/Home.vue
+++ components/Home.vue
@@ -1,17 +1,10 @@
 <template>
     <div>
-        <!-- <textarea v-model="inputStrings" placeholder="input here the original strings"></textarea> -->
         <v-textarea label="strings to convert" v-model="inputStrings" rows="10" variant="outlined" clearable></v-textarea>
     </div>
     <div>
         <div>This is ...</div>
-        <!-- <div>
-            <input type="radio" name="input_case" value="snake" />snake_case<br />
-            <input type="radio" name="input_case" value="kebab" />kebab-case<br />
-            <input type="radio" name="input_case" value="camel" />camelCase<br />
-            <input type="radio" name="input_case" value="pascal" />PascalCase<br />
-        </div> -->
-        <v-radio-group v-model="inputCase" @click="changeInputCase($event, '')">
+        <v-radio-group v-model="inputCase">
             <v-radio label="snake_case" value="snake"></v-radio>
             <v-radio label="kebab-case" value="kebab"></v-radio>
             <v-radio label="camelCase" value="camel"></v-radio>
@@ -20,12 +13,6 @@
     </div>
     <div>
         <div>Convert to ...</div>
-        <!-- <div>
-            <input type="radio" name="output_case" value="snake" />snake_case<br />
-            <input type="radio" name="output_case" value="kebab" />kebab-case<br />
-            <input type="radio" name="output_case" value="camel" />camelCase<br />
-            <input type="radio" name="output_case" value="pascal" />PascalCase<br />
-        </div> -->
         <v-radio-group v-model="outputCase">
             <v-radio label="snake_case" value="snake"></v-radio>
             <v-radio label="kebab-case" value="kebab"></v-radio>
@@ -34,7 +21,6 @@
         </v-radio-group>
     </div>
     <div>
-        <!-- <textarea v-model="outputStrings" readonly></textarea> -->
         <v-textarea label="conversion result" v-model="outputStrings" rows="10" variant="outlined" readonly></v-textarea>
     </div>
 </template>
@@ -101,8 +87,6 @@ const convert = (str: string, caseFrom: string, caseTo: string): string => {
 }
 const inputStrings = ref<string>("aaaBbb\ncccDdd\neeeF64G3iABC")
 const inputCase = ref<string>("camel")
-// inputStrings.value = "abcde"
-// inputCase.value = "kebab"
 const outputCase = ref<string>("snake")
 const outputStrings = computed(() => {
     return convert(inputStrings.value, inputCase.value, outputCase.value)
